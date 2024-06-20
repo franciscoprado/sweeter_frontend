e981bb15d46fa9b63783bb0a3a315f1185f8ac8a
Francisco Prado Rodrigues <franciscontato@gmail.com>
2024-06-20 09:50:35 -0300

    Fix no pipe

--- src/app/pipes/postagem.pipe.ts
+++ src/app/pipes/postagem.pipe.ts
@@ -6,10 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PostagemPipe implements PipeTransform {
   transform(value: string = '', ...args: unknown[]): unknown {
+    if (!value) return;
+    
     let texto = value.replace(
       /(https?:\/\/[^\s]+)/g,
       '<a href="$1" target="_blank">$1</a>'
     );
+    texto = texto.replaceAll('\r\n', '<br>');
     return texto;
   }
 }
